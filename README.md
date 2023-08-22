e142017691704e9a205e733354d78831f4203954
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2023-08-22 17:01:28 +0800

    新增 `shortcutio`

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Scriptio-user-scripts
-My user scripts for [Scriptio](https://github.com/PRO-2684/Scriptio)
+
+我的 [Scriptio](https://github.com/PRO-2684/Scriptio) 用户脚本。
+
+## [shortcutio](./shortcutio.js)
+
+添加常用快捷键，包括：
+
+- `F5` 刷新当前页面
+- `Esc` 关闭当前页面
+- `Enter` 聚焦到输入框（聊天页面）
---
+++ shortcutio.js
@@ -0,0 +1,89 @@
+// 添加一些常用的快捷键
+(function () {
+    // const data = {
+    //     "$shortcuts": {
+    //         "F5": () => { window.location.reload(); },
+    //     },
+    //     "tray-menu": {
+    //         "$shortcuts": {
+    //             "Escape": () => { window.close(); },
+    //         }
+    //     },
+    //     "main": {
+    //         "$shortcuts": {},
+    //         "message": {
+    //             "$shortcuts": {
+    //                 "Enter": () => {
+    //                     let editor = document.querySelector(".qq-editor .ck-content");
+    //                     if (editor) {
+    //                         editor.focus();
+    //                     }
+    //                 },
+    //             }
+    //         },
+    //         "contact": {
+    //             "$shortcuts": {},
+    //             "profile": {
+    //                 "$shortcuts": {}
+    //             },
+    //             "notify": {
+    //                 "$shortcuts": {}
+    //             }
+    //         },
+    //     },
+    //     "setting": {
+    //         "$shortcuts": {},
+    //         "settings": {
+    //             "$shortcuts": {},
+    //             // ...
+    //         }
+    //     }
+    // };
+    function wrapper(f) {
+        document.addEventListener("keydown", function (e) {
+            // 输入状态不触发
+            if (document.activeElement.tagName === "INPUT" || document.activeElement.tagName === "TEXTAREA"
+                || document.activeElement.getAttribute("contenteditable") === "true") {
+                return;
+            }
+            // 输入法下不触发
+            if (e.isComposing || e.keyCode === 229) {
+                return;
+            }
+            f(e);
+        });
+    }
+    const page = window.location.hash.slice(2).split("/")[0];
+    switch (page) {
+        case "main": {
+            wrapper(function (e) {
+                const p1 = window.location.hash.slice(2).split("/")[1];
+                if (e.key === "Enter") {
+                    if (p1 === "message") {
+                        let editor = document.querySelector(".qq-editor .ck-content");
+                        if (editor) {
+                            editor.focus();
+                        }
+                    }
+                }
+            });
+            break;
+        }
+        case "setting":
+            break;
+        case "tray-menu":
+            wrapper(function (e) {
+                if (e.key === "Escape") {
+                    window.close();
+                }
+            });
+            break;
+        default:
+            break;
+    }
+    wrapper(function (e) {
+        if (e.key === "F5") {
+            window.location.reload();
+        }
+    });
+})();
