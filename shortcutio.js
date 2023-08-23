37ce3ef9b635dcf1db5f93a8c4e575eaaf62bfa8
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2023-08-23 13:12:44 +0800

    shortcutio fix 2

--- shortcutio.js
+++ shortcutio.js
@@ -50,37 +50,53 @@
             if (e.isComposing || e.keyCode === 229) {
                 return;
             }
+            console.log(`[Shortcutio] ${e.key}`);
             f(e);
         });
     }
-    const page = window.location.hash.slice(2).split("/")[0];
-    switch (page) {
-        case "main": {
-            wrapper(function (e) {
-                const p1 = window.location.hash.slice(2).split("/")[1];
-                if (e.key === "Enter") {
-                    if (p1 === "message") {
-                        let editor = document.querySelector(".qq-editor .ck-content");
-                        if (editor) {
-                            editor.focus();
+    let page = window.location.hash.slice(2).split("/")[0];
+    function setupShortcuts() {
+        console.log(`[Shortcutio] page is: ${page}`);
+        console.log("[Shortcutio] Location:", window.location.href);
+        switch (page) {
+            case "main": {
+                wrapper(function (e) {
+                    const p1 = window.location.hash.slice(2).split("/")[1];
+                    if (e.key === "Enter") {
+                        if (p1 === "message") {
+                            let editor = document.querySelector(".qq-editor .ck-content");
+                            if (editor) {
+                                editor.focus();
+                            }
                         }
                     }
-                }
-            });
-            break;
+                });
+                break;
+            }
+            case "setting":
+                break;
+            case "tray-menu":
+                wrapper(function (e) {
+                    if (e.key === "Escape") {
+                        window.close();
+                    }
+                });
+                break;
+            default:
+                break;
         }
-        case "setting":
-            break;
-        case "tray-menu":
-        case "":
-            wrapper(function (e) {
-                if (e.key === "Escape") {
-                    window.close();
-                }
-            });
-            break;
-        default:
-            break;
+    }
+    if (page !== "blank") {
+        setupShortcuts();
+    } else {
+        function callback(e) {
+            page = window.location.hash.slice(2).split("/")[0];
+            if (page !== "blank") {
+                setupShortcuts();
+            }
+            document.removeEventListener("keydown", callback);
+        }
+        document.addEventListener("keydown", callback);
     }
     wrapper(function (e) {
         if (e.key === "F5") {
